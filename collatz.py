651ddc528becaf2e6fad8614f3d545243d1cb82f
Gerhard van der Linde <G00346778@gmit.ie>
2018-02-07 22:03:24 +0000

    Amend filenames and remove extra lines

--- collatz.py
+++ collatz.py
@@ -2,10 +2,8 @@
 # Gerhard van der Linde
 # Week 3 Assgnment - Collatz conjecture
 # https://en.wikipedia.org/wiki/Collatz_conjecture
-import matplotlib.pyplot as plt
-range=[]
+
 n=int(input("Please provide a starting number:"))
-nstart = n
 while n > 1:
     if n % 2 == 0: # even number
         n/=2
@@ -13,9 +11,3 @@ while n > 1:
         n*=3
         n+=1
     print("%i" %n)
-    range.append(int(n))
-plt.plot(range)
-plt.xlabel("iterations")
-plt.ylabel("Vlaue of n")
-plt.title("Collatz Conjecture starting at %i" % nstart)
-plt.show()
