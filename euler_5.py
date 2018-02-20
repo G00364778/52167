a52d1966f2a341b0abf295102c8a78f219e94815
Gerhard van der Linde <G00346778@gmit.ie>
2018-02-20 18:52:37 +0000

    improved version - reduce factors, explanation in coments

--- euler_5.py
+++ euler_5.py
@@ -5,19 +5,29 @@
 #
 # Write a Python program using for and range to calculate the smallest 
 # positive number that is evenly divisible by all of the numbers from 1 to 20.
-import time
+
+# ANSWER: Smallest value devisable by 1 to 20 is 232792560. (calculated in 46.820 s)
+
+# Optimisation steps were required to arrive at a workable solution.
+# The factors were reduce to 11,12,13,14,15,16,17,18,19,20 since the 
+# smaller values were all factors of the bigger ones in any case. Also
+# by incrementing by twenty every cycle reduced a lot of unnessesary attemps
+# that wold have failed in a test in any case, so this resulted in a 47 second
+# solution that was more acceptable.
+
+import time # to calculate the execution time
 
 foundanswer=False # set the starting condition of the value to false
-c=0 # Start the test value at zero
+c=20 # Start the test value at twenty
 maxval=20
 
 tstart=time.time()
 while foundanswer == False:
-    c+=1 # incrememyt the test value by one every cycle the answer is not found
-    testval=0 # reset the testval for the next test
-    for x in range(1,maxval+1):
-        testval+=c%x # if all the tests result on zero
+    c+=20 # increment the test value by twenty every cycle the answer is not found
+    testval=0 # reset the testval for the next test cyle
+    for x in range(11,maxval+1):
+        testval+=c%x # sum all the test values and test for zero
     if testval==0:
         foundanswer=True
         tend=time.time()
-        print('Smallest value devisable by 1 to %i is %. (calculated in %0.3f s)' %(maxval, c,tend-tstart))
+        print('Smallest value devisable by 1 to %i is %i. (calculated in %0.3f s)' %(maxval, c,tend-tstart))
